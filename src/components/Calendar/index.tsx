0fc4c75c045577f9f758676389108fd1e78ced37
Lamenkazu <333.erick@gmail.com>
2024-06-22 11:30:29 -0300

    ✨ Estrutura visual do calendário

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,64 @@
+import { CaretLeft, CaretRight } from 'phosphor-react'
+import React from 'react'
+
+import { getWeekDays } from '@/utils/get-week-days'
+
+import {
+  CalendarActions,
+  CalendarBody,
+  CalendarContainer,
+  CalendarDay,
+  CalendarHeader,
+  CalendarTitle,
+} from './styles'
+
+export const Calendar = () => {
+  const shortWeekDays = getWeekDays({ short: true })
+
+  return (
+    <CalendarContainer>
+      <CalendarHeader>
+        <CalendarTitle>
+          Junho <span>2024</span>
+        </CalendarTitle>
+
+        <CalendarActions>
+          <button>
+            <CaretLeft />
+          </button>
+          <button>
+            <CaretRight />
+          </button>
+        </CalendarActions>
+      </CalendarHeader>
+
+      <CalendarBody>
+        <thead>
+          <tr>
+            {shortWeekDays.map((weekDay) => (
+              <th key={weekDay}>{weekDay}.</th>
+            ))}
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+        </tbody>
+      </CalendarBody>
+    </CalendarContainer>
+  )
+}
---
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.page.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function ConfirmStep() {
+  return <div>ConfirmStep</div>
+}
