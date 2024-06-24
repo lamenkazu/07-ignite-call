11c01c2aa1615d7809e51bf9e9b3d14725a31b5d
Lamenkazu <333.erick@gmail.com>
2024-06-23 21:52:42 -0300

    👨‍💻✨ Preparando calendário

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -23,7 +23,12 @@ interface CalendarWeek {
 
 type CalendarWeeks = CalendarWeek[]
 
-export const Calendar = () => {
+interface CalendarProps {
+  selectedDate: Date | null
+  onDateSelected: (date: Date) => void
+}
+
+export const Calendar = ({ selectedDate, onDateSelected }: CalendarProps) => {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set('date', 1)
   })
@@ -80,7 +85,9 @@ export const Calendar = () => {
         return { date, disabled: true }
       }),
       ...daysInMonthArray.map((date) => {
-        return { date, disabled: false }
+        const disabled = date.endOf('day').isBefore(new Date())
+
+        return { date, disabled }
       }),
       ...nextMonthFillArray.map((date) => {
         return { date, disabled: true }
@@ -138,7 +145,10 @@ export const Calendar = () => {
             <tr key={week}>
               {days.map(({ date, disabled }) => (
                 <td key={date.toString()}>
-                  <CalendarDay disabled={disabled}>
+                  <CalendarDay
+                    onClick={() => onDateSelected(date.toDate())}
+                    disabled={disabled}
+                  >
                     {date.get('date')}
                   </CalendarDay>
                 </td>
